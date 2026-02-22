5084f751ffe4a22a40aacebed639797837a15c54
Wooseop-Ji <kiger7458@gmail.com>
2026-02-22 16:30:07 +0900

    구글 로그인 구현

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,15 @@
+// buildscript 블록 추가
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // 구글 서비스 플러그인 추가
+        classpath("com.google.gms:google-services:4.4.1")
+    }
+}
+
 allprojects {
     repositories {
         google()
